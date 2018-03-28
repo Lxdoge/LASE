63eff6165f7e5f3e863ce8ea25a6e3b6270a1941
Lxdoge <525115912@qq.com>
2018-03-28 19:01:03 +0800

    新建一个test场景专门用于存放机关 存放脚本organo，之后会写target脚本

---
+++ LaSend/Assets/Scripts/PlatformScripts/Organo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Organo : MonoBehaviour {
+    public bool timer;
+    public float[] contents;//一个数组,代表第x个机关触发后存在时长有多少，虽然想要设置成为-1时长无限但目前没有想到解决办法
+    public GameObject target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timer==true)
+        {
+
+        }
+	}
+}
