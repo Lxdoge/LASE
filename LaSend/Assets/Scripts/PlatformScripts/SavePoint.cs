922e61743bbea4ac0677004629145e4e0f2b3ad5
Lxdoge <525115912@qq.com>
2018-05-05 19:17:27 +0800

    存档和收集品逻辑

---
+++ LaSend/Assets/Scripts/PlatformScripts/SavePoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+public class SavePoint : MonoBehaviour {
+    [HideInInspector]
+    public bool [][]is_open;
+    public Sprite litpic;
+    public Animation lightSavePoint;
+    string path;
+    int scene;
+    public int c;
+	// Use this for initialization
+	void Start () {
+        is_open = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(is_open)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = litpic;
+            lightSavePoint.Play();
+        }
+	}
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        string ta = obj.gameObject.tag;
+        if(tag=="LPlayer"&&tag=="SPlayer")
+        {
+            this.is_open = true;
+        }
+    }
+}
