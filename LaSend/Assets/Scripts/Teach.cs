eb4aee61c1d25fa701ddf813032e5d61c81d9fff
Lxdoge <525115912@qq.com>
2018-06-14 05:52:03 +0800

    插件dotween Pro

---
+++ LaSend/Assets/Scripts/Teach.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+public class Teach : MonoBehaviour {
+    public DOTweenAnimation[] danima;
+    private static int can_load;
+	// Use this for initialization
+	void Start () {
+        can_load = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(can_load==3)
+        {
+            //切换场景
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        switch (col.gameObject.name)
+        {
+            case "S1":
+
+                can_load++;
+                break;
+            case "S2": can_load++; break;
+            case "end": can_load++; break;
+            default:break;
+        }
+    }
+
+    
+}
