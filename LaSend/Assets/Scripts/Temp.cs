d171dcceeb2005c69d3e7544cb1e61d4c04a8b86
Lxdoge <525115912@qq.com>
2018-06-29 12:32:06 +0800

    第三关的更改，求测，尤其是loading界面和第三关的特效，第三关的第一个恐龙骨头上画了地形

---
+++ LaSend/Assets/Scripts/Temp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour {
+    public Sprite pic;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "LPlayer" || collision.tag == "SPlayer")
+        {
+            this.GetComponent<SpriteRenderer>().sprite = pic;
+        }
+    }
+}
